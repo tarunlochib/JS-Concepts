befeb3aacc2a45083fc56242e7a582b08bb12fcb
Jerry <tarunlochib99@gmail.com>
2022-01-23 12:38:56 +0530

    changes made to polyfill file

--- polyfill_for_bind/index.js
+++ polyfill_for_bind/index.js
@@ -5,19 +5,20 @@ let names = {
     LastName: "Lochib"
 }
 
-let printName = function () {
-    console.log(this.firstName + " " + this.LastName)
+let printName = function (city, state) {
+    console.log(this.firstName + " " + this.LastName + " from " + city + "," + state)
 }
 
-let printMyName = printName.bind(names);
-printMyName();
+let printMyName = printName.bind(names, "Faridabad");
+printMyName("Haryana");
 
 Function.prototype.mybind = function (...args) {
-    let obj = this
-    return function () {
-        obj.call(args[0])
+    let obj = this,
+        params = args.slice(1);
+    return function (...args2) {
+        obj.apply(args[0], [...params, ...args2]);
     }
 }
 
-let printMyName2 = printName.mybind(names);
-printMyName2();
+let printMyName2 = printName.mybind(names, "Faridabad");
+printMyName2("Haryana");
